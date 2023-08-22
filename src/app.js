dec96366461c12ea235df465da386e7a519c40d2
FrancoRegg <138858381+FrancoRegg@users.noreply.github.com>
2023-08-22 07:01:52 +0000

    estilos realizados

--- src/app.js
+++ src/app.js
@@ -5,7 +5,12 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let numeros = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+let palos = [1, 2, 3, 4];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let numAleatorio = Math.floor(Math.random() * numeros.length);
+  let palosAleatorio = Math.floor(Math.random() * palos.length);
+
+  console.log(numeros[numAleatorio] + palos[palosAleatorio]);
 };
