f21f1c42432224ce526d631545a2d6b5b3450a4a
FrancoRegg <138858381+FrancoRegg@users.noreply.github.com>
2023-08-24 14:29:23 +0000

    Cambios de prueba

--- src/app.js
+++ src/app.js
@@ -5,12 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let numeros = [2, 3, 4, 5, 6, 7, 8, 9, 10];
-let palos = [1, 2, 3, 4];
+let numeros = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+let palos = ["♦", "♥", "♠", "♣"];
 
-window.onload = function() {
+window.onload = () => {
   let numAleatorio = Math.floor(Math.random() * numeros.length);
   let palosAleatorio = Math.floor(Math.random() * palos.length);
 
-  console.log(numeros[numAleatorio] + palos[palosAleatorio]);
+  document.getElementById("cards");
 };
